1740f556dde6625fc9a41f9de17381e963734c3d
muhammadazam28 <muhammadazam13128@gmail.com>
2026-05-12 11:20:17 +0700

    second commit

--- database/migrations/2026_05_11_071428_create_posts_table.php
+++ database/migrations/2026_05_11_071428_create_posts_table.php
@@ -10,15 +10,16 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-{
-    Schema::create('posts', function (Blueprint $table) {
+    {
+        Schema::create('posts', function (Blueprint $table) {
         $table->id();
         $table->string('image');
         $table->string('title');
         $table->text('content');
         $table->timestamps();
-    });
-}
+
+        });
+    }
 
     /**
      * Reverse the migrations.
